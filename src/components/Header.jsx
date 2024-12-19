2ee3812f83b10a61cd1764ffe7cc6625c2e71ff9
santoshjani31 <santosh.jani@gmail.com>
2024-12-19 21:17:52 +0000

    updated Task 10 view articles by topic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   const userAvatarURL =
     'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002';
+
+  // State to manage dropdown visibility
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  // Topics list for the dropdown
+  const topics = ['coding', 'cooking', 'football'];
 
   return (
     <header className='header'>
@@ -16,8 +22,25 @@ const Header = () => {
         />
       </div>
       <nav className='nav-bar'>
-        <Link to='/'>Home</Link> | <Link to='/topics'>Topics</Link> |
-        <Link to='/post-article'>Post Article</Link> |
+        <Link to='/'>Home</Link> |{/* Topics dropdown */}
+        <div
+          className='topics-dropdown'
+          onMouseEnter={() => setDropdownVisible(true)} // Show the dropdown when hovering over Topics
+          onMouseLeave={() => setDropdownVisible(false)} // Hide the dropdown when mouse leaves
+        >
+          <span>Topics</span>
+          {isDropdownVisible && (
+            <div className='dropdown-menu'>
+              {topics.map((topic) => (
+                <Link key={topic} to={`/topics/${topic}`}>
+                  {topic.charAt(0).toUpperCase() + topic.slice(1)}{' '}
+                  {/* Capitalize first letter */}
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+        | <Link to='/post-article'>Post Article</Link> |
         <Link to='/profile'>Profile</Link>
       </nav>
     </header>
