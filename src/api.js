df5efcf537a54634c29634896fe7a60a26ca6bf6
santoshjani31 <santosh.jani@gmail.com>
2024-12-30 12:02:41 +0000

    Deploy Commit

--- src/api.js
+++ src/api.js
@@ -2,10 +2,20 @@ import axios from 'axios';
 
 const BASE_URL = 'https://be-my-nc-news.onrender.com/api';
 
-export const fetchArticles = () =>
-  axios.get(`${BASE_URL}/articles`).then((response) => {
-    return response.data;
-  });
+export const fetchArticles = (sortBy = 'created_at', order = 'desc') => {
+  return axios
+    .get(`${BASE_URL}/articles`, {
+      params: {
+        sort_by: sortBy,
+        order: order,
+      },
+    })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error('Error fetching articles:', error);
+      throw new Error('Unable to fetch articles. Please try again later.');
+    });
+};
 
 export const fetchArticleDetails = (article_id) =>
   axios.get(`${BASE_URL}/articles/${article_id}`).then((response) => {
